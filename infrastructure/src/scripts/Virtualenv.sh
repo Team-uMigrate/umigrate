d0ea681bd1802ef84eb05d03751c5c54fa7eb2f8
Deandre Baker <49622234+deandrebaker@users.noreply.github.com>
2020-05-16 17:29:41 -0400

    Modified Virtualenv.sh

--- infrastructure/src/scripts/Virtualenv.sh
+++ infrastructure/src/scripts/Virtualenv.sh
@@ -5,7 +5,7 @@ SECRET_KEY=$1
 DATABASE_PASSWORD=$2
 REDIS_PASSWORD=$3
 SENDGRID_API_KEY=$4
-ALLOWED_HOSTS=$5
+DOMAIN_NAME=$5
 STAGE_ENVIRONMENT=$6
 
 # Create virtualenv
@@ -18,5 +18,5 @@ echo SECRET_KEY=$SECRET_KEY >> /home/umigrate/venv/.env
 echo DATABASE_PASSWORD=$DATABASE_PASSWORD >> /home/umigrate/venv/.env
 echo REDIS_PASSWORD=$REDIS_PASSWORD >> /home/umigrate/venv/.env
 echo SENDGRID_API_KEY=$SENDGRID_API_KEY >> /home/umigrate/venv/.env
-echo ALLOWED_HOSTS=$ALLOWED_HOSTS >> /home/umigrate/venv/.env
+echo DOMAIN_NAME=$DOMAIN_NAME >> /home/umigrate/venv/.env
 echo STAGE_ENVIRONMENT=$STAGE_ENVIRONMENT >> /home/umigrate/venv/.env

bf13e64b035b1e3907530b4503e92a59c4ef519b
Deandre Baker <deandreb54321@gmail.com>
2020-07-18 00:26:38 -0400

    refactored auth router to be a functional component

--- website/src/src/routers/AuthRouter.jsx
+++ website/src/src/routers/AuthRouter.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useContext } from "react";
 import { Switch, BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 import AdsPage from "../components/Advertisements";
 import SettingsPage from "../components/UserSettings";
@@ -10,46 +10,43 @@ import RegistrationPage from "../components/Register";
 import UsersPage from "../components/UsersPage";
 import AuthContext from "../contexts/AuthContext";
 
-class AuthRouter extends Component {
-  static contextType = AuthContext;
+const AuthRouter = () => {
+  const auth = useContext(AuthContext);
 
-  render() {
-    const { isAuthenticated } = this.context;
-
-    if(isAuthenticated === true) {
-      return (
-        <Router>
-          <Switch>
-            <Route path="/advertisements"><AdsPage /></Route>
-            <Route path="/community"><CommunityPage /></Route>
-            <Route path="/housing"><HousingPage /></Route>
-            <Route path="/settings"><SettingsPage /></Route>
-            <Route path="/messaging"><MessagingPage /></Route>
-            <Route path="/users"><UsersPage /></Route>
-            <Redirect from="/" to="/community" />
-          </Switch>
-        </Router>
-      )
-    }
-
-    else if(isAuthenticated === false) {
-      return (
-        <Router>
-          <Switch>
-            <Route path="/login"><LoginPage /></Route>
-            <Route path="/register"><RegistrationPage /></Route>
-            <Redirect from="/" to="/login" />;
-          </Switch>
-        </Router>
-      )
-    }
-
-    else {
-      return (
-        <h1>Please Wait.....</h1>
-      )
-    }
+  if(auth.isAuthenticated === true) {
+    return (
+      <Router>
+        <Switch>
+          <Route path="/advertisements"><AdsPage /></Route>
+          <Route path="/community"><CommunityPage /></Route>
+          <Route path="/housing"><HousingPage /></Route>
+          <Route path="/settings"><SettingsPage /></Route>
+          <Route path="/messaging"><MessagingPage /></Route>
+          <Route path="/users"><UsersPage /></Route>
+          // Todo: Set default page back to community
+          <Redirect from="/" to="/advertisements" />
+        </Switch>
+      </Router>
+    )
   }
-}
+
+  else if(auth.isAuthenticated === false) {
+    return (
+      <Router>
+        <Switch>
+          <Route path="/login"><LoginPage /></Route>
+          <Route path="/register"><RegistrationPage /></Route>
+          <Redirect from="/" to="/login" />;
+        </Switch>
+      </Router>
+    )
+  }
+
+  else {
+    return (
+      <h1>Please Wait.....</h1>
+    )
+  }
+};
 
 export default AuthRouter;

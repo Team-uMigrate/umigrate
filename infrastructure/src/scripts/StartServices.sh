fc1567a5273beef755f336efa84624bdd0acb57e
Deandre Baker <49622234+deandrebaker@users.noreply.github.com>
2020-08-18 22:09:07 -0400

    Reordered Daemon-reload

--- infrastructure/src/scripts/StartServices.sh
+++ infrastructure/src/scripts/StartServices.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Starting gunicorn, daphne, redis-server, and nginx
+systemctl daemon-reload
 systemctl start gunicorn.socket
 systemctl start gunicorn
 systemctl start daphne@{0..2}
 systemctl start redis-server
 systemctl start nginx
-systemctl daemon-reload

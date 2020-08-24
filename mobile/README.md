a9c794bdbfecf76255d484bdc029011218887299
Deandre Baker <49622234+deandrebaker@users.noreply.github.com>
2020-08-24 11:12:34 -0400

    Feature/create mobile (#39)
    * Created base mobile app
    
    * Modified App.js

---
+++ mobile/README.md
@@ -0,0 +1,5 @@
+# uMigrate
+Source code for the uMigrate web and mobile applications
+
+## Azure DevOps CI/CD Pipelines
+Check out our Build and Deploy pipelines for uMigrate at https://dev.azure.com/Team-uMigrate/umigrate/_build
---
+++ mobile/src/App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Welcome to uMigrate!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});

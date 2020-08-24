42b8490e173828ed1bd8b7e6696d9951c7142d4e
Deandre Baker <deandreb54321@gmail.com>
2020-08-24 11:10:45 -0400

    Modified App.js

--- mobile/src/App.js
+++ mobile/src/App.js
@@ -2,14 +2,16 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Welcome to uMigrate!</Text>
       <StatusBar style="auto" />
     </View>
   );
-}
+};
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {

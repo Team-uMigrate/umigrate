0bfe38f2767e5b7d5e6974b6fac96193c70e040b
joel22b <joel22b@gmail.com>
2020-09-12 18:44:21 -0400

    Modified Tests

---
+++ infrastructure/tests/console.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace system_tests
+{
+    class console
+    {
+        [Test]
+        public void consoleTest()
+        {
+            Console.WriteLine("This is a console Test");
+            var hi = "hello";
+        }
+    }
+}

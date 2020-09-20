86bab6454cf058814ec3f534d7159feefdb14441
Deandre Baker <deandreb54321@gmail.com>
2020-09-20 00:50:23 -0400

    Added interested and attending serializer methods to events serializer

--- api/src/events/serializers.py
+++ api/src/events/serializers.py
@@ -7,6 +7,8 @@ from rest_framework import serializers
 class EventSerializer(GenericPostSerializer):
     is_interested = serializers.SerializerMethodField()
     is_attending = serializers.SerializerMethodField()
+    interested = serializers.SerializerMethodField()
+    attending = serializers.SerializerMethodField()
 
     class Meta:
         model = Event
@@ -17,6 +19,12 @@ class EventSerializer(GenericPostSerializer):
 
     def get_is_attending(self, instance):
         return instance.attending_users.filter(id=self.context['request'].user.id).exists()
+
+    def get_interested(self, instance):
+        return instance.interested_users.count()
+
+    def get_attending(self, instance):
+        return instance.attending_users.count()
 
 
 # Serializes the event model

0620afa8440c30f2ac02e8fcd5bf7b57ce52ed83
Deandre Baker <49622234+deandrebaker@users.noreply.github.com>
2020-09-21 11:36:46 -0400

    Added url to swagger

--- api/src/umigrate/urls.py
+++ api/src/umigrate/urls.py
@@ -20,6 +20,7 @@ from django.urls import path, include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from .settings import ALLOWED_HOSTS, STAGE_ENVIRONMENT
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -28,6 +29,7 @@ schema_view = get_schema_view(
     ),
     public=False,
     permission_classes=(permissions.AllowAny,),
+    url=None if STAGE_ENVIRONMENT == 'local' else f'https://{ALLOWED_HOSTS[0]}',
 )
 
 

4d9cc8a8785d9a8537f945f2149e7f39a177441d
Shashank Anand <anandkannan.shashank62@gmail.com>
2020-09-23 19:22:12 -0400

    Put ListingContainer in the housing page

--- mobile/src/src/components/Housing/HousingPage.jsx
+++ mobile/src/src/components/Housing/HousingPage.jsx
@@ -1,25 +1,1 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import Header from "../common/Header";
-
-const HousingPage = () => {
-  return (
-    <View style={styles.container}>
-      <Header title="Housing" />
-      <Text style={styles.title}>Housing Page!</Text>
-    </View>
-  );
-};
-
-export default HousingPage;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#eeeeee",
-  },
-  title: {
-    alignSelf: "center",
-    marginTop: "80%",
-  },
-});
+import React, { useState } from "react";import { StyleSheet, Text, View } from "react-native";import Header from "../common/Header";import { ListingsEndpoint } from "../../utils/endpoints";import ListingContainer  from "./Listings/ListingsContainer";const HousingPage = () => {  return (    <View style={styles.container}>      <Header title="Housing" />      <ListingContainer/>    </View>  );};export default HousingPage;const styles = StyleSheet.create({  container: {    flex: 1,    backgroundColor: "#eeeeee",  },  title: {    alignSelf: "center",    marginTop: "80%",  },});

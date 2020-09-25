610620382af636a143fcbeed9a6d6f88fce218ef
Deandre Baker <49622234+deandrebaker@users.noreply.github.com>
2020-09-25 14:06:00 -0400

    Created detailed generic serializers that serialize tagged_users field

--- api/src/common/generics/generic_post_serializers.py
+++ api/src/common/generics/generic_post_serializers.py
@@ -7,7 +7,6 @@ from .generic_serializers import GenericSerializer
 class GenericPostSerializer(GenericSerializer):
     creator = BasicUserSerializer(read_only=True)
     liked_users = BasicUserSerializer(read_only=True, many=True)
-    tagged_users = BasicUserSerializer(read_only=True, many=True)
     is_liked = serializers.SerializerMethodField()
     likes = serializers.SerializerMethodField()
     comments = serializers.SerializerMethodField()
@@ -26,11 +25,15 @@ class GenericPostSerializer(GenericSerializer):
         return instance
 
 
+# Serializes a generic resource model with detail
+class GenericPostDetailSerializer(GenericPostSerializer):
+    tagged_users = BasicUserSerializer(read_only=True, many=True)
+
+
 # Serializes a generic comment model
 class GenericCommentSerializer(GenericSerializer):
     creator = BasicUserSerializer(read_only=True)
     liked_users = BasicUserSerializer(read_only=True, many=True)
-    tagged_users = BasicUserSerializer(read_only=True, many=True)
     is_liked = serializers.SerializerMethodField()
     likes = serializers.SerializerMethodField()
 
@@ -43,3 +46,8 @@ class GenericCommentSerializer(GenericSerializer):
     def create(self, validated_data):
         instance = self.Meta.model.objects.create(creator_id=self.context['request'].user.id, **validated_data)
         return instance
+
+
+# Serializes a generic resource model with detail
+class GenericCommentDetailSerializer(GenericCommentSerializer):
+    tagged_users = BasicUserSerializer(read_only=True, many=True)

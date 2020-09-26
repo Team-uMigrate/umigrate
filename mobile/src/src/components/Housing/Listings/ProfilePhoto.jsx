c525da7c5f56a229f76b9bb0dbbd1a8eef67074d
Shashank Anand <anandkannan.shashank62@gmail.com>
2020-09-26 15:24:37 -0400

    Add profilePhoto component
    This renders a profile photo depending on whether the user has uploaded one.

---
+++ mobile/src/src/components/Housing/Listings/ProfilePhoto.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import {Avatar} from "react-native-paper";
+import {Image} from "react-native";
+
+const ProfilePhoto = ({photo}) => {
+    const PROFILEIMAGESIZE = 45;
+
+    if(photo === null) {
+        return (
+            <Avatar.Icon size={PROFILEIMAGESIZE} icon={"folder"} />
+        );
+    }
+    else {
+        return (
+            <Avatar.Image size={PROFILEIMAGESIZE} source={{uri: photo}}/>
+        );
+    }
+}
+
+export default ProfilePhoto;

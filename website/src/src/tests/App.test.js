ea460b6eaf5f5cecaa2fa7dff648426ce0676bf8
Apsd1109 <46359439+Apsd1109@users.noreply.github.com>
2020-09-28 20:37:27 -0400

    revert test file

--- website/src/src/tests/App.test.js
+++ website/src/src/tests/App.test.js
@@ -4,4 +4,6 @@ import App from "../components/App";
 
 test("renders learn react link", () => {
   const { getByText } = render(<App />);
+  const linkElement = getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
 });

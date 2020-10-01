e8779de2821b03e7db18dd534f72c3ea65f0d835
Deandre Baker <49622234+deandrebaker@users.noreply.github.com>
2020-10-01 13:29:25 -0400

    Wrote basic selenium tests

--- infrastructure/tests/SystemTests/UnitTest1.cs
+++ infrastructure/tests/SystemTests/UnitTest1.cs
@@ -1,18 +1,48 @@
 using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
 
 namespace SystemTests
 {
     public class Tests
     {
+        String test_url = "https://www.google.com";
+
+        IWebDriver driver;
+
         [SetUp]
-        public void Setup()
+        public void start_Browser()
         {
+            // Local Selenium WebDriver
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
         }
 
         [Test]
-        public void Test1()
+        public void test_search()
         {
-            Assert.Pass();
+            driver.Url = test_url;
+
+            System.Threading.Thread.Sleep(2000);
+
+            IWebElement searchText = driver.FindElement(By.CssSelector("[name = 'q']"));
+
+            searchText.SendKeys("LambdaTest");
+
+            IWebElement searchButton = driver.FindElement(By.XPath("//div[@class='FPdoLc tfB0Bf']//input[@name='btnK']"));
+
+            searchButton.Click();
+
+            System.Threading.Thread.Sleep(6000);
+
+            Console.WriteLine("Test Passed");
+        }
+
+        [TearDown]
+        public void close_Browser()
+        {
+            driver.Quit();
         }
     }
 }

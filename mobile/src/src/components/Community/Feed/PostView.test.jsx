4058c9ab06604e3910b27d117bbfe8ee5f0aca55
Deandre Baker <deandreb54321@gmail.com>
2020-10-10 20:11:43 -0400

    Create basic test

---
+++ mobile/src/src/components/Community/Feed/PostView.test.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import PostView from "./PostView";
+
+describe("<PostView />", () => {
+  it("hi", () => {
+    expect(1).toBe(1);
+  });
+});

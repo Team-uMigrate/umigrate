cab7733fb3af8ef9a3901ddb4f485fcba76f9dee
Grace Nguyen <42276283+gracenng@users.noreply.github.com>
2020-10-19 18:09:45 -0400

    needs to reformat but its the modalVisible

--- mobile/src/src/components/Create/CreatePage.jsx
+++ mobile/src/src/components/Create/CreatePage.jsx
@@ -1,16 +1,36 @@
-import React from "react";
-import { Modal, StyleSheet, View } from "react-native";
+import React, { useContext, useState } from "react";
+import { Alert, Modal, StyleSheet, View } from "react-native";
 import Header from "../common/Header";
 import { TextInput, Text, Button } from "react-native-paper";
 import CreateModal from "./";
 
 const CreatePage = () => {
+  const [modalVisible, setModalVisible] = useState(true);
+  // setModalVisible(true);
   return (
     <View style={styles.container}>
       <Header title="Create" />
-      <Modal>
+      <Modal visible={modalVisible} style={styles.modal} transparent={true}>
         <View style={styles.modalView}>
           <Text>Where would you like to post...</Text>
+          <Button
+            compact={true}
+            style={styles.buttonStyle}
+            mode="contained"
+            title="Community"
+            onPress={() => Alert.alert("Simple Button pressed")}
+          >
+            Community
+          </Button>
+          <Button
+            compact={true}
+            style={styles.buttonStyle}
+            mode="contained"
+            title="Community"
+            onPress={() => Alert.alert("Simple Button pressed")}
+          >
+            Community
+          </Button>
         </View>
       </Modal>
     </View>
@@ -22,9 +42,16 @@ export default CreatePage;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#ffffff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: "#eeeeee",
+  },
+  buttonStyle: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  modal: {
+    width: 300,
+    height: 300,
+    margin: 0,
   },
   modalView: {
     margin: 20,

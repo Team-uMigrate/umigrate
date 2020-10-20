a69ebe24ea2ac35cb6e53a529b7688bddd6fe155
Deandre Baker <deandreb54321@gmail.com>
2020-10-20 19:25:44 -0400

    Improved efficiency of the create test data command

--- api/src/users/management/commands/createtestdata.py
+++ api/src/users/management/commands/createtestdata.py
@@ -16,12 +16,12 @@ USER_COUNT = 120
 ITEM_COUNT = 120
 
 
-def random_users(max_users=3, min_users=0, total_users=USER_COUNT):
+def random_users(users, max_users=3, min_users=0, total_users=USER_COUNT):
     num_of_users = random.randint(min_users, max_users)
     list_of_ids = []
     for i in range(num_of_users):
         list_of_ids = list_of_ids + [random.randint(1, total_users)]
-    return CustomUser.objects.filter(id__in=list_of_ids)
+    return users.filter(id__in=list_of_ids)
 
 
 class Command(BaseCommand):
@@ -52,8 +52,8 @@ class Command(BaseCommand):
 
         print('Assigning connected and blocked users...')
         for user in users:
-            connect_users = random_users()
-            blocked_users = random_users()
+            connect_users = random_users(users)
+            blocked_users = random_users(users)
             for a in connect_users:
                 user.connected_users.add(a)
             for b in blocked_users:
@@ -64,26 +64,26 @@ class Command(BaseCommand):
             rand_int1 = random.randint(0, 12)
             rand_int2 = random.randint(0, 12)
 
-            ad = AdFactory(liked_users=random_users(), tagged_users=random_users(),
-                           saved_users=random_users(), creator=users.get(id=random.randint(1, USER_COUNT)))
+            ad = AdFactory(liked_users=random_users(users), tagged_users=random_users(users),
+                           saved_users=random_users(users), creator=users.get(id=random.randint(1, USER_COUNT)))
 
-            event = EventFactory(liked_users=random_users(), tagged_users=random_users(),
-                                 saved_users=random_users(), creator=users.get(id=random.randint(1, USER_COUNT)),
-                                 interested_users=random_users(), attending_users=random_users())
+            event = EventFactory(liked_users=random_users(users), tagged_users=random_users(users),
+                                 saved_users=random_users(users), creator=users.get(id=random.randint(1, USER_COUNT)),
+                                 interested_users=random_users(users), attending_users=random_users(users))
 
             job = JobFactory(creator=users.get(id=random.randint(1, USER_COUNT)))
 
-            listing = ListingFactory(liked_users=random_users(), tagged_users=random_users(),
-                                     saved_users=random_users(), creator=users.get(id=random.randint(1, USER_COUNT)))
+            listing = ListingFactory(liked_users=random_users(users), tagged_users=random_users(users),
+                                     saved_users=random_users(users), creator=users.get(id=random.randint(1, USER_COUNT)))
 
-            room = RoomFactory(members=random_users(), creator=users.get(id=random.randint(1, USER_COUNT)))
+            room = RoomFactory(members=random_users(users), creator=users.get(id=random.randint(1, USER_COUNT)))
 
             for j in range(rand_int1):
-                message = MessageFactory(liked_users=random_users(), tagged_users=random_users(),
+                message = MessageFactory(liked_users=random_users(users), tagged_users=random_users(users),
                                          creator=users.get(id=random.randint(1, USER_COUNT)), room=room)
 
-            poll = PollFactory(liked_users=random_users(), tagged_users=random_users(),
-                               saved_users=random_users(), creator=users.get(id=random.randint(1, USER_COUNT)))
+            poll = PollFactory(liked_users=random_users(users), tagged_users=random_users(users),
+                               saved_users=random_users(users), creator=users.get(id=random.randint(1, USER_COUNT)))
 
             for m in range(rand_int2):
                 option = OptionFactory(creator=users.get(id=random.randint(1, USER_COUNT)), poll=poll)
@@ -91,20 +91,20 @@ class Command(BaseCommand):
                 for n in range(rand_int1):
                     vote = VoteFactory(creator=users.get(id=random.randint(1, USER_COUNT)), option=option)
 
-            post = PostFactory(liked_users=random_users(), tagged_users=random_users(),
-                               saved_users=random_users(), creator=users.get(id=random.randint(1, USER_COUNT)))
+            post = PostFactory(liked_users=random_users(users), tagged_users=random_users(users),
+                               saved_users=random_users(users), creator=users.get(id=random.randint(1, USER_COUNT)))
 
             postings = [ad, event, listing, poll, post]
 
             for x in range(rand_int1):
-                comment = CommentFactory(liked_users=random_users(), tagged_users=random_users(),
-                                         saved_users=random_users(),
+                comment = CommentFactory(liked_users=random_users(users), tagged_users=random_users(users),
+                                         saved_users=random_users(users),
                                          creator=users.get(id=random.randint(1, USER_COUNT)),
                                          content_object=postings[random.randint(0, len(postings) - 1)])
 
                 for y in range(rand_int2):
-                    reply = ReplyFactory(liked_users=random_users(), tagged_users=random_users(),
-                                         saved_users=random_users(),
+                    reply = ReplyFactory(liked_users=random_users(users), tagged_users=random_users(users),
+                                         saved_users=random_users(users),
                                          creator=users.get(id=random.randint(1, USER_COUNT)), comment=comment)
 
             if (i + 1) % 10 == 0:

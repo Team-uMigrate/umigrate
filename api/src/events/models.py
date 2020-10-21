f8399ee26bb8a1b3ba1311ff1edf4feacac6f7fe
Jerry Bi <j39bi@edu.uwaterloo.ca>
2020-10-21 08:42:06 -0500

    Added Validation for start and end dates

--- api/src/events/models.py
+++ api/src/events/models.py
@@ -14,3 +14,13 @@ class Event(AbstractPostModel, GenericPhotoCollection):
     interested_users = models.ManyToManyField(to=CustomUser, related_name='interested_event_set', blank=True)
     attending_users = models.ManyToManyField(to=CustomUser, related_name='attending_event_set', blank=True)
     location = models.CharField(max_length=100, blank=True, default='123 Goose st, Waterloo, ON')
+
+    def save(self, *args, **kwargs):
+        self.clean()
+        super().save(*args, **kwargs)
+
+    def clean(self):
+        if self.start_datetime is None:
+            raise ValidationError({'start_datetime': _('Start Date cannot be null')})
+        if self.start_datetime > self.end_datetime:
+            raise ValidationError({'end_datetime': _('End date before start date')})

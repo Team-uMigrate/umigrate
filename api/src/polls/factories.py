3827e6c8e7a21dc9cac82fa9d6417b026b21dc20
Deandre Baker <deandreb54321@gmail.com>
2020-10-20 22:44:48 -0400

    reduced sentence length again

--- api/src/polls/factories.py
+++ api/src/polls/factories.py
@@ -13,7 +13,7 @@ class OptionFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Option
 
-    content = factory.Faker('sentence')
+    content = factory.Faker('sentence', nb_words=3)
     creator = factory.SubFactory(UserFactory)
     poll = factory.SubFactory(PollFactory)
 

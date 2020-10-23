cff2ddcf84fc1de733626f2bb628282a4520e454
Deandre Baker <deandreb54321@gmail.com>
2020-10-22 22:30:38 -0400

    Revert "Revert "Comment out most_liked_comment and reply because it broke server""
    This reverts commit 5a5f3a3c

--- api/src/common/abstract_serializers.py
+++ api/src/common/abstract_serializers.py
@@ -1,4 +1,4 @@
-from django.db.models.functions import Length
+# from django.db.models.functions import Length
 from comments.serializers import CommentDetailSerializer
 from users.serializers import BasicUserSerializer
 from rest_framework import serializers
@@ -14,7 +14,7 @@ class AbstractModelSerializer(ModelSerializerExtension):
     is_saved = serializers.SerializerMethodField()
     likes = serializers.SerializerMethodField()
     comments = serializers.SerializerMethodField()
-    most_liked_comment = serializers.SerializerMethodField()
+    # most_liked_comment = serializers.SerializerMethodField()
 
     def get_is_liked(self, instance):
         return instance.liked_users.filter(id=self.context['request'].user.id).exists()
@@ -28,14 +28,14 @@ class AbstractModelSerializer(ModelSerializerExtension):
     def get_comments(self, instance):
         return instance.comments.count()
 
-    def get_most_liked_comment(self, instance):
-        most_liked_comment = instance.comments.order_by(Length('liked_users').desc(), '-datetime_created').first()
+    # def get_most_liked_comment(self, instance):
+    #     most_liked_comment = instance.comments.order_by(Length('liked_users').desc(), '-datetime_created').first()
 
-        if most_liked_comment is None:
-            return None
+    #     if most_liked_comment is None:
+    #         return None
 
-        most_liked_comment_serializer = CommentDetailSerializer(most_liked_comment, context=self.context)
-        return most_liked_comment_serializer.data
+    #     most_liked_comment_serializer = CommentDetailSerializer(most_liked_comment, context=self.context)
+    #     return most_liked_comment_serializer.data
 
     def create(self, validated_data):
         validated_data['creator'] = self.context['request'].user

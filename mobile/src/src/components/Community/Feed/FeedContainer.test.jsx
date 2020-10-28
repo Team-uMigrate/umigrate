2ac162f9a3d480108e5dc6010ab73906ababccbc
Deandre Baker <49622234+deandrebaker@users.noreply.github.com>
2020-10-28 18:20:05 -0400

    Add enzyme to packages

--- mobile/src/src/components/Community/Feed/FeedContainer.test.jsx
+++ mobile/src/src/components/Community/Feed/FeedContainer.test.jsx
@@ -1,21 +1,29 @@
 import React from "react";
 import { render, waitFor } from "react-native-testing-library";
+import { shallow } from "enzyme";
 import FeedContainer from "./FeedContainer";
-import { setupMockEndpoint } from "../../../utils/mockEndpoints";
-import { mockPost } from "../../../utils/mockData";
+import { mockEvent, mockPost } from "../../../utils/mockData";
 
+// Todo: We need to check that the FeedContainer renders the correct amount of PostView components onto the DOM,
+// and we need to test that the state changes correctly when api calls to fetch more postings are made when scrolling to
+// The bottom.
+// We should also test the custom logic for getting the right posts from the api
 describe("<FeedContainer />", () => {
-  let screen = null;
+  let wrapper = null;
+  let feedContainer = null;
   beforeEach(() => {
-    setupMockEndpoint();
+    wrapper = shallow(<FeedContainer />);
+    feedContainer = wrapper.instance();
     screen = render(<FeedContainer />);
   });
 
-  it("should contain 3 items", async () => {
-    await waitFor(() =>
-      expect(
-        screen.queryAllByText(mockPost.creator.preferred_name).length
-      ).toBe(3)
-    );
+  it("Should get a list of posts", () => {
+    expect(wrapper.state("posts")).toHaveLength(0);
+
+    feedContainer.getPosts();
+
+    waitFor(() => {
+      expect(wrapper.state("posts")).toHaveLength(3);
+    });
   });
 });

47a2a8556d96d93968624fdf97128c5e0bc82556
Terence Wong <terencewong.work@gmail.com>
2020-10-27 22:17:27 -0400

    created base api view

--- api/src/common/abstract_api_views.py
+++ api/src/common/abstract_api_views.py
@@ -2,6 +2,7 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.generics import ListAPIView
 from common.abstract_models import IsCreatorOrReadOnly
 
 
@@ -24,3 +25,15 @@ class AbstractModelViewSet(ModelViewSet):
         if self.request.method == "GET":
             return self.detail_serializer_class
         return self.serializer_class
+
+class AbstractSavedView(ListAPIView):
+    queryset = None
+    app_string = None 
+    def get_queryset(self):
+        #user.saved_posts_set.all()
+        return getattr(self.request.user, f"saved_{app_string}_set").all()
+
+    def post(self, request, *args, **kwargs):
+        
+        
+        

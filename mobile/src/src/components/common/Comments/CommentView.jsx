ab553d8f99851886d393bb5bcbfa197997de3acc
Shashank Anand <anandkannan.shashank62@gmail.com>
2020-10-27 21:30:55 -0400

    Implement basic comment view

--- mobile/src/src/components/common/Comments/CommentView.jsx
+++ mobile/src/src/components/common/Comments/CommentView.jsx
@@ -1,12 +1,45 @@
 import React from "react";
-import { StyleSheet } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
+import { Avatar } from "react-native-paper";
 
-const CommentView = () => {
-
-}
+const CommentView = ({ id, content }) => {
+  return (
+    <View style={styles.commentView}>
+      <Text>Shashank!!!</Text>
+      <View style={{ flexDirection: "row" }}>
+        <View style={{ marginRight: "2.5%" }}>
+          <Avatar.Icon size={30} icon={"account"} />
+        </View>
+        <View style={styles.contentContainer}>
+          <Text>{"Comment " + id}</Text>
+        </View>
+        <View style={styles.timestampView}>
+          <Text style={styles.timestamp}>2:12 pm</Text>
+        </View>
+      </View>
+    </View>
+  );
+};
 
 export default CommentView;
 
 const styles = StyleSheet.create({
-
+  commentView: {
+    margin: 5,
+    marginLeft: 10,
+  },
+  contentContainer: {
+    borderRadius: 100,
+    backgroundColor: "#EBEBEB",
+    padding: 5,
+  },
+  timestampView: {
+    marginLeft: 2,
+    alignSelf: "flex-end",
+  },
+  timestamp: {
+    color: "gray",
+    fontSize: 10,
+    paddingBottom: 5,
+  },
 });
--- mobile/src/src/components/common/Comments/CommentsContainer.jsx
+++ mobile/src/src/components/common/Comments/CommentsContainer.jsx
@@ -1,22 +1,59 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, FlatList, Text, View } from "react-native";
+import CommentView from "./CommentView";
 
 class CommentsContainer extends Component {
-  state = {};
+  state = {
+    comments: [
+      { id: 1 },
+      { id: 2 },
+      { id: 3 },
+      { id: 4 },
+      { id: 5 },
+      { id: 6 },
+    ],
+    nextPage: 1,
+    nextPageExists: true,
+  };
 
-  CommentsContainer({ postId }) {
-    this.state.postId = postId;
+  constructor(props) {
+    super(props);
+    this.state.postId = props.postId;
+    //this.state.comments.concat(this.props.navigation.fetchComments());
+    console.log(this.state.comments);
   }
+
+  renderItem = ({ item }) => {
+    return <CommentView {...item} />;
+  };
 
   // TODO use this.props.navigation.fetchComments() to load and render comments
 
   render() {
     return (
-      <View>
-        <Text>This is a comment</Text>
+      <View style={styles.commentsContainer}>
+        <FlatList
+          data={this.state.comments}
+          keyExtractor={(item, i) => i.toString()}
+          renderItem={this.renderItem}
+        />
       </View>
     );
   }
 }
 
 export default CommentsContainer;
+
+const styles = StyleSheet.create({
+  commentsContainer: {
+    flexDirection: "column",
+    backgroundColor: "white",
+    margin: 10,
+    flex: 1,
+  },
+  commentView: {
+    flex: 2,
+    alignSelf: "center",
+    margin: 50,
+  },
+});

e8451ed1a6f189185642999bc7214d52037f17c5
Apsd1109 <46359439+Apsd1109@users.noreply.github.com>
2020-10-31 22:01:45 -0400

    oops

--- infrastructure/deployment/scripts/EnableServices.sh
+++ infrastructure/deployment/scripts/EnableServices.sh
@@ -3,7 +3,7 @@
 # Enable daphne, gunicorn, nginx
 # These can always be run, won't harm anything
 systemctl daemon-reload
-systemctl enable daphne
+systemctl enable daphne@{0..2}
 systemctl enable gunicorn.socket
 systemctl enable redis-server
 systemctl reload nginx

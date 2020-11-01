edc9aac30b611e95369627bb3ebfb728d9a68bc8
Apsd1109 <Apsd1109@hotmail.ca>
2020-10-31 22:32:10 -0400

    fix createsu script

--- api/deployment/scripts/CreateSU.sh
+++ api/deployment/scripts/CreateSU.sh
@@ -5,4 +5,4 @@ SUPERUSER_PASSWORD=$2
 
 # Create super user
 # Create only if SU doesn't exist (do in python)
-export $(egrep -v '^#' /home/umigrate/venv/.env | xargs) && python3 /home/umigrate/api/manage.py shell -c "from users.models import CustomUser; users = CustomUser.objects.all().filter(is_superuser=True, email="$REF_TO_EMAIL"); CustomUser.objects.create_superuser('$(SUPERUSER_EMAIL)', '$(SUPERUSER_PASSWORD)') if len(users) == 0 else None"
+export $(egrep -v '^#' /home/umigrate/venv/.env | xargs) && python3 /home/umigrate/api/manage.py shell -c "from users.models import CustomUser; users = CustomUser.objects.all().filter(is_superuser=True, email='$(SUPERUSER_EMAIL)'); CustomUser.objects.create_superuser('$(SUPERUSER_EMAIL)', '$(SUPERUSER_PASSWORD)') if len(users) == 0 else None"

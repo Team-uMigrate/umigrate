053217dd5b185184fffe69c510bc1cda03b8e7a0
Apsd1109 <46359439+Apsd1109@users.noreply.github.com>
2020-11-04 22:10:21 -0500

    modify "create dir"

--- infrastructure/deployment/scripts/DownloadAzCopy.sh
+++ infrastructure/deployment/scripts/DownloadAzCopy.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-mkdir /home/umigrate/maintenance/azcopy
+# Create azcopy folder
+DIR="/home/umigrate/maintenance/azcopy"
+if [ ! -d "$DIR" ]
+	then
+		mkdir /home/umigrate/maintenance/azcopy
+fi
 cd /home/umigrate/maintenance/azcopy
-wget -O azcopy_v10.tar.gz https://aka.ms/downloadazcopy-v10-linux && tar -xf azcopy_v10.tar.gz --strip-components=1
 
+# Download AzCopy tool
+wget -O azcopy_v10.tar.gz https://aka.ms/downloadazcopy-v10-linux && tar -xf azcopy_v10.tar.gz --strip-components=1

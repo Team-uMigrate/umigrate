74066c1b11b12c2670f28e5699e05e78f8d40327
Apsd1109 <Apsd1109@hotmail.ca>
2020-11-04 22:01:32 -0500

    create local folders for azcopy

--- infrastructure/deployment/scripts/DownloadAzCopy.sh
+++ infrastructure/deployment/scripts/DownloadAzCopy.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+mkdir /home/umigrate/maintenance/azcopy
+cd /home/umigrate/maintenance/azcopy
 wget -O azcopy_v10.tar.gz https://aka.ms/downloadazcopy-v10-linux && tar -xf azcopy_v10.tar.gz --strip-components=1
 

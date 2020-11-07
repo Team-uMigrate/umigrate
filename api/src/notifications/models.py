c6d5998672b61eed160148f528445194e10891bc
Deandre Baker <deandreb54321@gmail.com>
2020-11-07 15:04:02 -0500

    Create notification model

--- api/src/notifications/models.py
+++ api/src/notifications/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from users.models import CustomUser
 
-# Create your models here.
+
+class Notification(models.Model):
+    id = models.AutoField(primary_key=True)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey("content_type", "object_id")
+    datetime_created = models.DateTimeField(auto_now_add=True)
+    recievers = models.ManyToManyField(
+        to=CustomUser, related_name="received_notification_set", blank=True
+    )
+    viewers - models.ManyToManyField(
+        to=CustomUser, related_name="viewed_notification_set"
+    )
+

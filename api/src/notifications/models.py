37eac2cfc07ad2adfc76bbdaff1dfb0e3b433bd9
Deandre Baker <deandreb54321@gmail.com>
2020-11-07 15:04:11 -0500

    Create device model

--- api/src/notifications/models.py
+++ api/src/notifications/models.py
@@ -15,3 +15,11 @@ class Notification(models.Model):
         to=CustomUser, related_name="viewed_notification_set"
     )
 
+
+class Device(models.Model):
+    id = models.AutoField(primary_key=True)
+    owner = models.ForeignKey(
+        to=Comment, related_name="device_set", on_delete=models.CASCADE
+    )
+    expo_push_token = models.CharField(max_length=50)
+    datetime_created = models.DateTimeField(auto_now_add=True)

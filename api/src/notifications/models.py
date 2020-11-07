db2ee0fa8c990067de519a4362e570b3197250d5
Deandre Baker <deandreb54321@gmail.com>
2020-11-07 15:36:26 -0500

    Fixed bugs with notification models.py

--- api/src/notifications/models.py
+++ api/src/notifications/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from users.models import CustomUser
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 
 
 class Notification(models.Model):
@@ -11,15 +13,15 @@ class Notification(models.Model):
     recievers = models.ManyToManyField(
         to=CustomUser, related_name="received_notification_set", blank=True
     )
-    viewers - models.ManyToManyField(
-        to=CustomUser, related_name="viewed_notification_set"
+    viewers = models.ManyToManyField(
+        to=CustomUser, related_name="viewed_notification_set", blank=True
     )
 
 
 class Device(models.Model):
     id = models.AutoField(primary_key=True)
     owner = models.ForeignKey(
-        to=Comment, related_name="device_set", on_delete=models.CASCADE
+        to=CustomUser, related_name="device_set", on_delete=models.CASCADE
     )
     expo_push_token = models.CharField(max_length=50)
     datetime_created = models.DateTimeField(auto_now_add=True)

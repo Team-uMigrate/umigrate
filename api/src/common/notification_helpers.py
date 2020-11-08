8563433ea50e9c919945dca0c005fc5e8103e42b
Deandre Baker <deandreb54321@gmail.com>
2020-11-08 17:45:12 -0500

    Send push notifications when notification is created

--- api/src/common/notification_helpers.py
+++ api/src/common/notification_helpers.py
@@ -1,5 +1,5 @@
 from django.contrib.contenttypes.models import ContentType
-from exponent_server_sdk import PushMessage
+from exponent_server_sdk import PushMessage, PushClient
 from comments.models import Comment, Reply
 from common.abstract_models import AbstractPostModel
 from messaging.models import Message
@@ -16,11 +16,27 @@ def create_tagged_user_notification(
             content = f"{created_data.creator.preferred_name} has tagged you in an {content_type.model}!"
         else:
             content = f"{created_data.creator.preferred_name} has tagged you in a {content_type.model}!"
-        tagged_notification = Notification(
+        notification = Notification(
             content=content,
             content_type=content_type,
             object_id=created_data.id,
             creator_id=created_data.creator.id,
         )
-        tagged_notification.save()
-        tagged_notification.receivers.add(*created_data.tagged_users.all())
+        notification.save()
+        notification.receivers.add(*created_data.tagged_users.all())
+        send_push_notifications(notification)
+
+
+def send_push_notifications(notification: Notification) -> None:
+    push_messages = []
+    for receiver in notification.receivers.all():
+        for device in receiver.device_set.all():
+            push_messages.append(
+                PushMessage(
+                    to=device.expo_push_token,
+                    body=notification.content,
+                    data=dict(NotificationSerializer(notification).data),
+                )
+            )
+
+    PushClient().publish_multiple(push_messages)

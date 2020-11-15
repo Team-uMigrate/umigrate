c85cd7187195f144f46d99e73b5bb1d49bb008cc
Adrian Salvador <59633359+adrian-salvador@users.noreply.github.com>
2020-11-14 19:50:30 -0500

    Update Redis.sh
    Add sudo (prefix) to commands

--- infrastructure/deployment/scripts/Redis.sh
+++ infrastructure/deployment/scripts/Redis.sh
@@ -5,5 +5,5 @@ REDIS_PASSWORD=$1
 
 # Set Redis password and configure Redis to be supervised by systemd
 # TODO: fix supervised no to change to supervised systemd
-sed -i "s/# requirepass foobared/requirepass $REDIS_PASSWORD/g" /etc/redis/redis.conf
-sed -i "s/supervised no/supervised systemd/g" /etc/redis/redis.conf
+sudo sed -i "s/# requirepass foobared/requirepass $REDIS_PASSWORD/g" /etc/redis/redis.conf
+sudo sed -i "s/supervised no/supervised systemd/g" /etc/redis/redis.conf

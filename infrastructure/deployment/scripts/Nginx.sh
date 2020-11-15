80e4eb38cf13d82f4828e684defd38af5b4a5a31
Adrian Salvador <59633359+adrian-salvador@users.noreply.github.com>
2020-11-14 19:49:55 -0500

    Update Nginx.sh
    Add sudo (prefix) to commands

--- infrastructure/deployment/scripts/Nginx.sh
+++ infrastructure/deployment/scripts/Nginx.sh
@@ -4,4 +4,4 @@ DOMAIN_NAME=$1
 # Symlinking 
 # To prevent extra junk in pipelines, we can do ln -sf
 # For the future, hardcode dev.umigrate.ca and umigrate.ca
-ln -sf /etc/nginx/sites-available/$DOMAIN_NAME /etc/nginx/sites-enabled
+sudo ln -sf /etc/nginx/sites-available/$DOMAIN_NAME /etc/nginx/sites-enabled

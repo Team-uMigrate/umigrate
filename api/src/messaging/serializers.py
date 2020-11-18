778bd5589ea2bffecac6544f14ce80d4ec153d25
rwywawn <rwywawnw@gmail.com>
2020-11-18 11:42:54 -0500

    fixed spelling

--- api/src/messaging/serializers.py
+++ api/src/messaging/serializers.py
@@ -14,7 +14,7 @@ class RoomSerializer(ModelSerializerExtension):
         return ModelSerializerExtension.create(self, validated_data)
 
 
-# Serialiezes the previous replied message
+# Serializes the previous replied message
 class BasicMessageSerializer(ModelSerializerExtension):
     creator = BasicUserSerializer(read_only=True)
 

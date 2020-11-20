9f5b071de763ebb2aed25cc3f39c37addbec8075
Apsd1109 <46359439+Apsd1109@users.noreply.github.com>
2020-11-19 19:28:06 -0500

    remove spaces that somehow mess things up this is so dumb

--- infrastructure/deployment/scripts/Postgres.sh
+++ infrastructure/deployment/scripts/Postgres.sh
@@ -9,7 +9,7 @@ if [ -z $(sudo -u postgres psql -lqt | cut -d\| -f 1 | grep umigratedb) ]; then
 	-c "CREATE DATABASE umigratedb;" \
 	-c "CREATE USER umigrate WITH PASSWORD '$DATABASE_PASSWORD';" \
 	-c "ALTER ROLE umigrate SET client_encoding TO 'utf8';" \
-   	-c "ALTER ROLE umigrate SET default_transaction_isolation TO 'read committed';" \   	
+   	-c "ALTER ROLE umigrate SET default_transaction_isolation TO 'read committed';" \
 	-c "ALTER ROLE umigrate SET timezone TO 'UTC';" \
 	-c "GRANT ALL PRIVILEGES ON DATABASE umigratedb TO umigrate;"
 fi

40d3b4d4bb1b6ef86bc31080d531c5a1fce53437
Deandre Baker <deandreb54321@gmail.com>
2020-11-21 17:05:05 -0500

    Use feed container in pages

--- mobile/src/src/components/Housing/HousingPage.jsx
+++ mobile/src/src/components/Housing/HousingPage.jsx
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 import Header from '../common/Header';
-import ListingContainer from './Listings/ListingsContainer';
 import CreateModal from '../Create/CreateModal';
+import { ListingsEndpoint } from '../../utils/endpoints';
+import ListingView from './Listings/ListingView';
+import FeedContainer from '../common/FeedContainer';
+
+const endpoints = [ListingsEndpoint];
+const itemViews = [(item) => <ListingView {...item} />];
 
 const HousingPage = ({ navigation }) => {
+  const [listingFilters, setListingFilters] = useState({});
   return (
     <View style={styles.container}>
       <Header title="Housing" />
-      <ListingContainer />
+      <FeedContainer
+        endpoints={endpoints}
+        itemViews={itemViews}
+        filtersList={[listingFilters]}
+      />
       <CreateModal navigation={navigation} />
     </View>
   );

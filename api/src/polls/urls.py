8c964b7caec463c8efd4a82a83c0bc7e7b4f7e60
Terence Wong <terencewong.work@gmail.com>
2020-11-20 22:07:54 -0500

    formatting

--- api/src/polls/urls.py
+++ api/src/polls/urls.py
@@ -1,13 +1,19 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
-from .api_views import PollViewSet, PollLike, OptionListCreate, VoteListCreate, SavedPoll
+from .api_views import (
+    PollViewSet,
+    PollLike,
+    OptionListCreate,
+    VoteListCreate,
+    SavedPoll,
+)
 
 # Polls url patterns
 router = DefaultRouter(trailing_slash=False)
 router.register(r"", PollViewSet, basename="polls")
 urlpatterns = router.urls + [
     path("like", PollLike.as_view()),
-    path("saved", SavedPoll.as_view())
+    path("saved", SavedPoll.as_view()),
     path("options/", OptionListCreate.as_view()),
     path("options/votes/", VoteListCreate.as_view()),
 ]

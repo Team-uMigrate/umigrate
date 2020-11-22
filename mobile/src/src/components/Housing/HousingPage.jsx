a4caa0a6d79441972fc89af882a004728be9225a
Deandre Baker <49622234+deandrebaker@users.noreply.github.com>
2020-11-22 16:52:50 -0500

    Feature/feed container tests (#352)
    * Move scrollRef to pages
    
    * Add test for fetch items
    
    * Made setup function for feed container

--- mobile/src/src/components/Housing/HousingPage.jsx
+++ mobile/src/src/components/Housing/HousingPage.jsx
@@ -1,16 +1,20 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 import Header from '../common/Header';
 import CreateModal from '../Create/CreateModal';
 import { ListingsEndpoint } from '../../utils/endpoints';
 import ListingView from './Listings/ListingView';
 import FeedContainer from '../common/FeedContainer';
+import { useScrollToTop } from '@react-navigation/native';
 
 const endpoints = [ListingsEndpoint];
 const itemViews = [(item) => <ListingView {...item} />];
 
 const HousingPage = ({ navigation }) => {
   const [listingFilters, setListingFilters] = useState({});
+  const ref = useRef(null);
+
+  useScrollToTop(ref);
 
   return (
     <View style={styles.container}>
@@ -19,6 +23,7 @@ const HousingPage = ({ navigation }) => {
         endpoints={endpoints}
         itemViews={itemViews}
         filtersList={[listingFilters]}
+        scrollRef={ref}
       />
       <CreateModal navigation={navigation} />
     </View>

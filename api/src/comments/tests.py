2913d35734033d67bf974492e61460acb54fd3dc
Deandre Baker <49622234+deandrebaker@users.noreply.github.com>
2020-11-23 08:52:00 -0500

    Add test for replies

--- api/src/comments/tests.py
+++ api/src/comments/tests.py
@@ -107,6 +107,8 @@ class ReplyTestCase(AbstractAPITestCase, APITestCase):
         self.assert_equal = self.assertEqual
         self.assert_list_equal = self.assertListEqual
         self.endpoint = "/api/comments/replies/"
+        self.order_ascending = "?ordering=datetime_created"
+        self.order_descending = "?ordering=-datetime_created"
         self.model_class = Reply
         self.serializer_class = ReplySerializer
         self.detail_serializer_class = ReplyDetailSerializer
@@ -142,3 +144,44 @@ class ReplyTestCase(AbstractAPITestCase, APITestCase):
 
     def test_destroy(self):
         AbstractAPITestCase.test_destroy(self)
+
+    def test_datetime_created(self):
+        # standard list test with modified endpoint
+        response = self.api_client.get(self.endpoint + self.order_ascending)
+        self.assert_equal(
+            response.status_code,
+            status.HTTP_200_OK,
+            "Status code should be 200. " f"Error: {response.data}",
+        )
+        results = response.data["results"]
+        items = self.model_class.objects.all()
+        self.assert_equal(
+            len(results), len(items), f"There should be ${len(items)} results"
+        )
+
+        ascendingSortedList = sorted(results, key=lambda x: x["id"])
+        self.assert_equal(
+            results,
+            ascendingSortedList,
+            "Results should be sorted in ascending chronological order.",
+        )
+
+    def test_reverse_datetime_created(self):
+        response = self.api_client.get(self.endpoint + self.order_descending)
+        self.assert_equal(
+            response.status_code,
+            status.HTTP_200_OK,
+            "Status code should be 200. " f"Error: {response.data}",
+        )
+        results = response.data["results"]
+        items = self.model_class.objects.all()
+        self.assert_equal(
+            len(results), len(items), f"There should be ${len(items)} results"
+        )
+
+        descendingSortedList = sorted(results, key=lambda x: x["id"], reverse=True)
+        self.assert_equal(
+            results,
+            descendingSortedList,
+            "Results should be sorted in descending chronological order.",
+        )

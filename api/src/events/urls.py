36e10fd725c8af3cce2ad53ca625a3c3e9d7c56a
Brian <brianmguyen@gmail.com>
2020-11-22 21:16:01 -0500

    Implemented api views for other endpoints

--- api/src/events/urls.py
+++ api/src/events/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
-from .api_views import EventViewSet, EventLike, EventInterestedUser, EventAttendingUser
+from .api_views import EventViewSet, EventLike, EventLikes, EventInterestedUser, EventAttendingUser
 
 # Events url patterns
 router = DefaultRouter(trailing_slash=False)
 router.register(r"", EventViewSet, basename="events")
 urlpatterns = router.urls + [
     path("like", EventLike.as_view()),
+    path("likes", EventLikes.as_view()),
     path("interested", EventInterestedUser.as_view()),
     path("attending", EventAttendingUser.as_view()),
 ]

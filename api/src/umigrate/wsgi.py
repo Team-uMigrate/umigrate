1bca96ccfafc29fd1ba4af08fee63882cf992721
Deandre Baker <deandreb54321@gmail.com>
2020-11-27 18:15:32 -0500

    move sentry configuration from setttings.py to asgi and wsgi

--- api/src/umigrate/wsgi.py
+++ api/src/umigrate/wsgi.py
@@ -10,7 +10,21 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from sentry_sdk.integrations.asgi import SentryAsgiMiddleware
+from sentry_sdk.integrations.django import DjangoIntegration
+import sentry_sdk
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "umigrate.settings")
 
+# Sentry Initialization
+sentry_sdk.init(
+    dsn="https://a4946255ae774d7e9c0dd8b5adfa9526@o442315.ingest.sentry.io/5413903",
+    # integrations=[DjangoIntegration()],
+    # traces_sample_rate=1.0,
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    # send_default_pii=True,
+)
+
 application = get_wsgi_application()
+application = SentryAsgiMiddleware(application)

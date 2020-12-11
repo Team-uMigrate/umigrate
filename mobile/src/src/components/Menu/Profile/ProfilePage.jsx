ac6d97eab1efcaccc4fece65f2fa12b089e10629
Drasti <52871085+d36mehta@users.noreply.github.com>
2020-12-11 18:02:54 -0500

    Feature/290 edit profile page (#365)
    * edit page without images saving and tests
    
    * fixes after merging master
    
    * saving info to async, tests, fixed issue with updating on other pages, added scroll so entire page shows on android
    
    * fixes after merging master (removing region)
    
    * re-added region but renamed to community
    
    * fix test
    
    * commenting out image file useState
    
    * added underscore to prevProps in componentDidUpdate
    
    * some fixes and ios14 datetimepicker update fix
    
    Co-authored-by: Deandre Baker <49622234+deandrebaker@users.noreply.github.com>

--- mobile/src/src/components/Menu/Profile/ProfilePage.jsx
+++ mobile/src/src/components/Menu/Profile/ProfilePage.jsx
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Image,
+  ScrollView,
+  Platform,
+} from 'react-native';
 import { Avatar, Button } from 'react-native-paper';
-import { Choices, ProfileEndpoint } from '../../../utils/endpoints';
+import { Choices, getUserData } from '../../../utils/endpoints';
 import Header from '../../common/Header';
 import ProfileComponents from './ProfileComponents';
 class ProfilePage extends Component {
@@ -9,19 +17,18 @@ class ProfilePage extends Component {
 
   constructor(props) {
     super(props);
-    this.getProfile();
   }
 
-  getProfile = () => {
-    ProfileEndpoint.get(
-      (response) => {
-        this.setState({ user: response.data });
-      },
-      (error) => {
-        console.log(error);
-        console.log(error.response);
-      }
-    );
+  componentDidMount = async () => {
+    const userData = await getUserData();
+    this.setState({ user: userData });
+  };
+
+  componentDidUpdate = async (_prevProps, prevState) => {
+    if (prevState != this.state) {
+      const userData = await getUserData();
+      this.setState({ user: userData });
+    }
   };
 
   render() {
@@ -46,63 +53,68 @@ class ProfilePage extends Component {
             </TouchableOpacity>
           </View>
         </View>
-        <View style={styles.pfInfo}>
-          <View>
-            <ProfileComponents
-              label="Preferred name"
-              val={this.state.user.preferred_name}
-            />
-          </View>
-          <View style={styles.rows}>
-            <ProfileComponents
-              label="First name"
-              val={this.state.user.first_name}
-            />
-            <ProfileComponents
-              label="Last name"
-              val={this.state.user.last_name}
-              row={true}
-            />
-          </View>
-          <View>
-            <ProfileComponents label="Email" val={this.state.user.email} />
-            <ProfileComponents
-              label="Phone"
-              val={this.state.user.phone_number}
-            />
-          </View>
-          <View style={styles.rows}>
-            <ProfileComponents
-              label="Pronoun"
-              val={Choices.pronouns[this.state.user.pronouns]}
-            />
-            <ProfileComponents
-              label="Birthday"
-              val={this.state.user.birthday}
-              row={true}
-            />
-          </View>
-          <View>
-            <ProfileComponents
-              label="Region"
-              val={Choices.regions[this.state.user.region]}
-            />
-            <ProfileComponents
-              label="Program"
-              val={Choices.programs[this.state.user.enrolled_program]}
-            />
-            <ProfileComponents
-              label="Current Term"
-              val={Choices.terms[this.state.user.current_term]}
-            />
-          </View>
-        </View>
-        <Button
-          style={styles.editButton}
-          onPress={() => this.props.navigation.navigate('EditProfile')}
+        <ScrollView
+          style={{ flex: 1, marginHorizontal: 1 }}
+          scrollEnabled={Platform.OS == 'android'}
         >
-          <Text style={styles.editButtonText}>Edit profile</Text>
-        </Button>
+          <View style={styles.pfInfo}>
+            <View>
+              <ProfileComponents
+                label="Preferred name"
+                val={this.state.user.preferred_name}
+              />
+            </View>
+            <View style={styles.rows}>
+              <ProfileComponents
+                label="First name"
+                val={this.state.user.first_name}
+              />
+              <ProfileComponents
+                label="Last name"
+                val={this.state.user.last_name}
+                row={true}
+              />
+            </View>
+            <View>
+              <ProfileComponents label="Email" val={this.state.user.email} />
+              <ProfileComponents
+                label="Phone"
+                val={this.state.user.phone_number}
+              />
+            </View>
+            <View style={styles.rows}>
+              <ProfileComponents
+                label="Pronoun"
+                val={Choices.pronouns[this.state.user.pronouns]}
+              />
+              <ProfileComponents
+                label="Birthday"
+                val={this.state.user.birthday}
+                row={true}
+              />
+            </View>
+            <View>
+              <ProfileComponents
+                label="Community"
+                val={Choices.communities[this.state.user.community]}
+              />
+              <ProfileComponents
+                label="Program"
+                val={Choices.programs[this.state.user.enrolled_program]}
+              />
+              <ProfileComponents
+                label="Current Term"
+                val={Choices.terms[this.state.user.current_term]}
+              />
+            </View>
+          </View>
+          <Button
+            style={styles.editButton}
+            onPress={() => this.props.navigation.navigate('EditProfile')}
+          >
+            <Text style={styles.editButtonText}>Edit profile</Text>
+          </Button>
+        </ScrollView>
       </View>
     );
   }

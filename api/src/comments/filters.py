c64819908e89f2c1b2ae492577612862383556b4
Deandre Baker <49622234+deandrebaker@users.noreply.github.com>
2020-12-29 14:33:49 -0500

    Add filter for comments

---
+++ api/src/comments/filters.py
@@ -0,0 +1,43 @@
+from django_filters import rest_framework as filters
+from .models import Comment, Reply
+
+
+# A filter set class for the Comment model
+class CommentFilterSet(filters.FilterSet):
+    min_datetime_created = filters.DateTimeFilter(
+        field_name="datetime_created", lookup_expr="gte"
+    )
+    max_datetime_created = filters.DateTimeFilter(
+        field_name="datetime_created", lookup_expr="lte"
+    )
+
+    class Meta:
+        model = Comment
+        fields = [
+            "creator",
+            "min_datetime_created",
+            "max_datetime_created",
+            "community",
+            "content_type",
+            "object_id",
+        ]
+
+
+# A filter set class for the Reply model
+class ReplyFilterSet(filters.FilterSet):
+    min_datetime_created = filters.DateTimeFilter(
+        field_name="datetime_created", lookup_expr="gte"
+    )
+    max_datetime_created = filters.DateTimeFilter(
+        field_name="datetime_created", lookup_expr="lte"
+    )
+
+    class Meta:
+        model = Reply
+        fields = [
+            "creator",
+            "min_datetime_created",
+            "max_datetime_created",
+            "community",
+            "comment",
+        ]

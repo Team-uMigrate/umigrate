5f4c41d0b3d769aabb86c8a5990cea5abbf695cb
Deandre Baker <49622234+deandrebaker@users.noreply.github.com>
2020-12-29 14:25:35 -0500

    Create filters for shared items

--- api/src/listings/filters.py
+++ api/src/listings/filters.py
@@ -2,29 +2,28 @@ from django_filters import rest_framework as filters
 from .models import Listing
 
 
+# A filter set class for the Listing model
 class ListingFilter(filters.FilterSet):
-    # More on lookup_expr syntax: https://django-filter.readthedocs.io/en/latest/ref/filters.html
-    # https://docs.djangoproject.com/en/3.0/ref/models/querysets/#field-lookups
-    # gte = greater than or equal to
-    # lte = less than or equal to
-
+    min_datetime_created = filters.DateTimeFilter(
+        field_name="datetime_created", lookup_expr="gte"
+    )
+    max_datetime_created = filters.DateTimeFilter(
+        field_name="datetime_created", lookup_expr="lte"
+    )
     min_price = filters.NumberFilter(field_name="price", lookup_expr="gte")
     max_price = filters.NumberFilter(field_name="price", lookup_expr="lte")
-    min_date = filters.DateTimeFilter(field_name="datetime_created", lookup_expr="gte")
-    max_date = filters.DateTimeFilter(field_name="datetime_created", lookup_expr="lte")
-    location = filters.CharFilter(field_name="location", lookup_expr="icontains")
 
     class Meta:
         model = Listing
         fields = [
+            "creator",
+            "min_datetime_created",
+            "max_datetime_created",
+            "community",
+            "category",
             "min_price",
             "max_price",
-            "min_date",
-            "max_date",
-            "datetime_created",
-            "creator",
-            "category",
-            "price",
-            "title",
-            "location",
+            "season",
+            "year",
+            "quantity",
         ]

682d6f064d5c28c58ac42c87a5a43a6715675834
Deandre Baker <49622234+deandrebaker@users.noreply.github.com>
2020-12-30 02:17:12 -0500

    Create error context

---
+++ app/src/src/contexts/ErrorContext.jsx
@@ -0,0 +1,27 @@
+import React, { Component, createContext } from 'react';
+
+const ErrorContext = createContext();
+
+// A context provider that stores the error state
+class ErrorContextProvider extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: false,
+      setMessage: (message) => {
+        this.setState({ message: message });
+      },
+    };
+  }
+
+  render() {
+    return (
+      <ErrorContext.Provider value={this.state}>
+        {this.props.children}
+      </ErrorContext.Provider>
+    );
+  }
+}
+
+export { ErrorContextProvider };
+export default ErrorContext;

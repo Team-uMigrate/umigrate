19c8abb45770670107b31679890d116841f6ee2a
Deandre Baker <49622234+deandrebaker@users.noreply.github.com>
2020-12-30 13:28:45 -0500

    Move loading view to it's own screen

---
+++ app/src/src/screens/LoadingScreen.jsx
@@ -0,0 +1,40 @@
+import React, { useContext, useEffect } from 'react';
+import {
+  getAuthToken,
+  ProfileEndpoint,
+  removeAuthToken,
+  setAuthToken,
+} from '../utils/endpoints';
+import AuthContext from '../contexts/AuthContext';
+import { ActivityIndicator, Text, View } from 'react-native';
+
+const LoadingScreen = () => {
+  const auth = useContext(AuthContext);
+
+  useEffect(() => {
+    (async () => {
+      const token = await getAuthToken();
+
+      // Make request to Profile endpoint and set isAuthenticated to true if successful and false otherwise
+      if (token != null) {
+        await setAuthToken(token);
+        try {
+          await ProfileEndpoint.get();
+          auth.setIsAuthenticated(true);
+        } catch (error) {
+          await removeAuthToken();
+          auth.setIsAuthenticated(false);
+        }
+      } else auth.setIsAuthenticated(false);
+    })();
+  });
+
+  return (
+    <View style={styles.waitContainer}>
+      <Text>Please Wait</Text>
+      <ActivityIndicator size="large" />
+    </View>
+  );
+};
+
+export default LoadingScreen;

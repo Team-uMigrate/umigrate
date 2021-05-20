952923f6f954daf2d7eccce95b535e3ad6d74026
drastimehta <drastiimehtaa@gmail.com>
2021-05-20 09:14:15 -0400

    clean up date formatting a little bit

--- app/src/src/components/common/FormatDate/toYearMonthDayTimeInNumbers.jsx
+++ app/src/src/components/common/FormatDate/toYearMonthDayTimeInNumbers.jsx
@@ -1,12 +1,9 @@
-// Takes in a Date object and returns an EST date string to send to the API
-// Format in YYYY-MM-DDThh:mm:ss.uuuuuuZ (ex: 1960-01-01T12:00:00.000000Z)
+import ToYearMonthDayInNumbers from './toYearMonthDayInNumbers';
+
+/* Takes in a Date object and returns an EST date string to send to the API
+   Format in YYYY-MM-DDThh:mm:ss.uuuuuuZ (ex: 1960-01-01T12:00:00.000000Z) */
 const toYearMonthDayTimeInNumbers = ({ date }) => {
-  let dateString = date.toLocaleDateString('en-CA', {
-    timezone: 'EST',
-    year: 'numeric',
-    month: 'numeric',
-    day: 'numeric',
-  });
+  let dateString = ToYearMonthDayInNumbers({ selectedValue: date });
   let timeString = date.toLocaleTimeString('en-CA', {
     timezone: 'EST',
     hour12: false,

d2f60377fb27219d65515f0832accf37e0739daf
Nihal Potdar <Nihalpot2002@gmail.com>
2021-05-31 15:56:51 -0600

    fix formatting

--- api/src/messaging/migrations/0004_alter_message_content_type.py
+++ api/src/messaging/migrations/0004_alter_message_content_type.py
@@ -7,14 +7,19 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('contenttypes', '0002_remove_content_type_name'),
-        ('messaging', '0003_auto_20210531_0258'),
+        ("contenttypes", "0002_remove_content_type_name"),
+        ("messaging", "0003_auto_20210531_0258"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='message',
-            name='content_type',
-            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL, to='contenttypes.contenttype'),
+            model_name="message",
+            name="content_type",
+            field=models.ForeignKey(
+                blank=True,
+                null=True,
+                on_delete=django.db.models.deletion.SET_NULL,
+                to="contenttypes.contenttype",
+            ),
         ),
     ]

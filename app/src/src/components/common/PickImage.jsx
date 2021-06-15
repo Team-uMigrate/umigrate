070dfcdc9922d3d4b135bfa69daa968443023da9
drastimehta <drastiimehtaa@gmail.com>
2021-06-14 21:39:16 -0400

    make aspect ratio 1:1 so ios and android are the same

--- app/src/src/components/common/PickImage.jsx
+++ app/src/src/components/common/PickImage.jsx
@@ -7,7 +7,7 @@ const pickImage = async ({ set }) => {
   const result = await ImagePicker.launchImageLibraryAsync({
     mediaTypes: ImagePicker.MediaTypeOptions.Images,
     allowsEditing: true,
-    aspect: [4, 3],
+    aspect: [1, 1],
     quality: 1,
   });
 

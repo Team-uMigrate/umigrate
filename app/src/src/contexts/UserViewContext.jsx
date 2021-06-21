e73382921cf9e8a2f3aa2a71ffe272f5710b70dc
joryclements <joryclements@rogers.com>
2021-06-21 19:11:20 -0400

    added context provider

---
+++ app/src/src/contexts/UserViewContext.jsx
@@ -0,0 +1,22 @@
+import React, { useState, createContext } from 'react';
+
+const UserViewContext = createContext();
+
+// A context provider that stores the user whose profile is to be shown
+const UserViewContextProvider = ({ children }) => {
+  const [user, setUser] = useState(null)
+
+  return (
+    <UserViewContext.Provider
+      value={{
+        user: user,
+        setUser: setUser,
+      }}
+    >
+      {children}
+    </UserViewContext.Provider>
+  );
+};
+
+export { UserViewContextProvider };
+export default UserViewContext;

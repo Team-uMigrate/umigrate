5b1248213add0c003fb8eb6a4225f8579d4cf022
Allie Dong <allied9420@gmail.com>
2021-06-22 20:45:39 -0400

    Added color change of buttons, removed properties and text change

--- app/src/src/components/views/EventView.jsx
+++ app/src/src/components/views/EventView.jsx
@@ -8,6 +8,7 @@ import ImageCollectionView from './ImageCollectionView';
 import GradientButton from 'react-native-gradient-buttons';
 import moment from 'moment';
 import { communities, contentTypes, prices } from '../../utils/choices';
+import RadialGradient from 'react-native-radial-gradient';
 
 const EventView = (event) => {
   const {
@@ -69,12 +70,13 @@ const EventView = (event) => {
         <ImageCollectionView photos={photos} />
         <View style={styles.buttonContainer}>
           <GradientButton
-            compact={true}
             style={styles.buttonStyleAttend}
-            mode={is_attending ? 'contained' : 'outlined'}
-            title={is_attending ? 'Attending' : 'Attending'}
-            gradientBegin={is_attending ? null : '#483FAB'}
-            textStyle={styles.bodyText}
+            gradientBegin={is_attending ? '#ffffff' : '#483FAB'}
+            gradientEnd={is_attending ? '#ffffff' : '#FF668B'}
+            textStyle={[
+              styles.bodyText,
+              { color: is_attending ? '#483FAB' : '#ffffff' },
+            ]}
             radius={10}
             onPressAction={async () => {
               await EventsEndpoint.setAttending(event.id, !event.is_attending);
@@ -87,17 +89,19 @@ const EventView = (event) => {
               });
             }}
           >
-            {is_attending ? 'Attending' : 'Attend?'}
+            Attending
           </GradientButton>
 
           <GradientButton
-            compact={true}
             style={styles.buttonStyleInterest}
-            mode={is_interested ? 'contained' : 'outlined'}
-            title={is_interested ? 'Uninterest' : 'Interested?'}
             textStyle={styles.bodyText}
             radius={10}
-            gradientBegin={is_interested ? null : '#483FAB'}
+            gradientBegin={is_interested ? '#ffffff' : '#483FAB'}
+            gradientEnd={is_interested ? '#ffffff' : '#FF668B'}
+            textStyle={[
+              styles.bodyText,
+              { color: is_interested ? '#483FAB' : '#ffffff' },
+            ]}
             onPressAction={async () => {
               await EventsEndpoint.setInterested(
                 event.id,
@@ -112,7 +116,7 @@ const EventView = (event) => {
               });
             }}
           >
-            {is_interested ? 'Uninterest' : 'Interested?'}
+            Interested
           </GradientButton>
         </View>
         <CommentBar
@@ -173,11 +177,18 @@ const styles = StyleSheet.create({
     flex: 1,
     marginLeft: 0,
     marginRight: '4%',
+    //backgroundColor: "'radialGradient(608.33% 2909.72% at 97.66% -441.67%, #FF668B 0%, rgba(72, 63, 171, 0) 100%), #483FAB',",
+    borderRadius: 11,
+    borderWidth: 1,
+    borderColor: '#483FAB',
   },
   buttonStyleInterest: {
     height: 36,
     flex: 1,
     marginRight: 0,
+    borderRadius: 11,
+    borderWidth: 1,
+    borderColor: '#483FAB',
   },
   buttonContainer: {
     flexDirection: 'row',

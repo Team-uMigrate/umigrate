e84ce21aabee6a43df48c67c79b96588f4f4ebbc
Vansh Mago <vmago@uwaterloo.ca>
2021-06-22 23:35:50 -0400

    Addressed requested changes

--- app/src/src/screens/createItem/CreateMarketScreen.jsx
+++ app/src/src/screens/createItem/CreateMarketScreen.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import {  View, Text } from 'react-native';
-import {sharedItemstyles} from '../../stylesheets/createItem/createItem.jsx';
+import {sharedItemStyles} from '../../stylesheets/createItem/createItem.jsx';
 
 const CreateMarketScreen = () => {
   return (
-    <View styles={sharedItemstyles}>
+    <View styles={sharedItemStyles}>
       <Text>Create a post in market!</Text>
     </View>
   );

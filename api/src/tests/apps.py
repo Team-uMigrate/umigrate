94004ff34d5895742b3cc65a9bf76f2be9db9518
Deandre Baker <49622234+deandrebaker@users.noreply.github.com>
2021-07-01 13:11:24 -0400

    Formatting

--- api/src/tests/apps.py
+++ api/src/tests/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class TestsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'tests'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "tests"

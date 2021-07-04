c3f8ff10dfcda87371b8a35df01bba0c529a19a0
Deandre Baker <49622234+deandrebaker@users.noreply.github.com>
2021-07-04 13:09:24 -0400

    Feature/notification helpers tests part 1 (#515)
    * Move notification helpers to notifications folder
    
    * Rename to helpers
    
    * Rename to utils
    
    * Rename to test_notifications_utils
    
    * Remove redundant words from tests
    
    * Check if receivers is not empty

--- api/src/notifications/utils.py
+++ api/src/notifications/utils.py
@@ -1,4 +1,5 @@
-from types import SimpleNamespace
+from typing import List
+from django.http import HttpRequest
 from users.models import CustomUser
 from django.contrib.contenttypes.models import ContentType
 from django.db.models.query import QuerySet
@@ -17,8 +18,8 @@ def create_tagged_users_notification(
     A function that sends push notifications to all tagged users for a shared item.
     """
 
-    tagged_users = created_data.tagged_users.all()
-    if len(tagged_users) > 0:
+    tagged_users: QuerySet[CustomUser] = created_data.tagged_users.all()
+    if tagged_users.exists():
         content_type = ContentType.objects.get_for_model(created_data)
 
         # todo: remove hardcoded list of content types that start with vowels and check if first letter is a vowel
@@ -71,17 +72,18 @@ def create_message_notification(
     A function that sends push notifications to a user when they receive a message.
     """
 
-    content_type = ContentType.objects.get_for_model(message)
-    content = f"{sender.preferred_name} sent you a message"
-    notification = Notification(
-        content=content,
-        content_type=content_type,
-        object_id=sender.id,
-        creator_id=sender.id,
-    )
-    notification.save()
-    notification.receivers.add(*receivers)
-    send_push_notifications(notification)
+    if receivers.exists():
+        content_type = ContentType.objects.get_for_model(message)
+        content = f"{sender.preferred_name} sent you a message"
+        notification = Notification(
+            content=content,
+            content_type=content_type,
+            object_id=sender.id,
+            creator_id=sender.id,
+        )
+        notification.save()
+        notification.receivers.add(*receivers)
+        send_push_notifications(notification)
 
 
 def create_liked_shared_item_notification(
@@ -91,7 +93,7 @@ def create_liked_shared_item_notification(
     A function that sends push notifications to the owner of a shared item when it is liked.
     """
 
-    owner = liked_data.creator
+    owner: CustomUser = liked_data.creator
     content_type = ContentType.objects.get_for_model(liked_data)
     content = f"{liker.preferred_name} liked your {content_type.model}!"
     notification = Notification.objects.create(
@@ -110,10 +112,10 @@ def send_push_notifications(notification: Notification) -> None:
     A function that sends push notifications to the receivers of a notification object.
     """
 
-    push_messages = []
+    push_messages: List[PushMessage] = []
     for receiver in notification.receivers.all():
         for device in receiver.devices.all():
-            request = SimpleNamespace()
+            request = HttpRequest()
             request.user = notification.creator
             push_messages.append(
                 PushMessage(
@@ -127,4 +129,5 @@ def send_push_notifications(notification: Notification) -> None:
                 )
             )
 
-    PushClient().publish_multiple(push_messages)
+    if len(push_messages) > 0:
+        PushClient().publish_multiple(push_messages)

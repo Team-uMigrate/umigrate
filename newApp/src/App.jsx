bbf2386456867fa8aa4eff761d2167968a51c8dc
Deandre Baker <49622234+deandrebaker@users.noreply.github.com>
2021-07-04 17:20:29 -0400

    Fix formatting

--- newApp/src/App.jsx
+++ newApp/src/App.jsx
@@ -1,9 +1,9 @@
-import React from "react";
-import { StatusBar } from "react-native";
-import { AuthContextProvider } from "./src/contexts/AuthContext";
-import { Provider as PaperProvider } from "react-native-paper";
-import AuthNavigator from "./src/navigators/AuthNavigator";
-import { ErrorContextProvider } from "./src/contexts/ErrorContext";
+import React from 'react';
+import { StatusBar } from 'react-native';
+import { AuthContextProvider } from './src/contexts/AuthContext';
+import { Provider as PaperProvider } from 'react-native-paper';
+import AuthNavigator from './src/navigators/AuthNavigator';
+import { ErrorContextProvider } from './src/contexts/ErrorContext';
 
 /**
  * Renders the app.

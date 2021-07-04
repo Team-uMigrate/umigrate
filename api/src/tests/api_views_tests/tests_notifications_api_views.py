679f12d30eb9725d9ae4c84bc2743fa84d167cfc
Deandre Baker <49622234+deandrebaker@users.noreply.github.com>
2021-07-04 00:24:10 -0400

    Add tests for ViewedReceivedNotifications.get_queryset

--- api/src/tests/api_views_tests/tests_notifications_api_views.py
+++ api/src/tests/api_views_tests/tests_notifications_api_views.py
@@ -1,3 +1,4 @@
+from django.db.models import QuerySet
 from django.http import HttpRequest
 from model_bakery import baker
 from rest_framework import status
@@ -14,13 +15,29 @@ class ViewedReceivedNotificationsTestCase(APITestCase):
         pass
 
     def test_get_query_set_received_and_viewed_notifications(self):
-        pass
+        # Arrange
+        request = HttpRequest()
+        request.user = baker.make("CustomUser")
+        user = baker.make("CustomUser")
+        received_notification: Notification = baker.make("Notification", creator=user)
+        received_notification.receivers.add(request.user)
+        viewed_notification: Notification = baker.make("Notification", creator=user)
+        viewed_notification.viewers.add(request.user)
+        api_view = ViewedReceivedNotifications(request=request)
+
+        # Act
+        queryset: QuerySet[Notification] = api_view.get_queryset()
+
+        # Arrange
+        self.assertTrue(queryset.filter(id=received_notification.id).exists())
+        self.assertTrue(queryset.filter(id=viewed_notification.id).exists())
 
     def test_post_set_viewed(self):
         # Arrange
         request = HttpRequest()
         request.user = baker.make("CustomUser")
-        notification: Notification = baker.make("Notification", creator=request.user)
+        user = baker.make("CustomUser")
+        notification: Notification = baker.make("Notification", creator=user)
         notification.receivers.add(request.user)
         request.data = {"received_notifications": [notification.id]}
         api_view = ViewedReceivedNotifications()
@@ -37,7 +54,8 @@ class ViewedReceivedNotificationsTestCase(APITestCase):
         # Arrange
         request = HttpRequest()
         request.user = baker.make("CustomUser")
-        notification: Notification = baker.make("Notification", creator=request.user)
+        user = baker.make("CustomUser")
+        notification: Notification = baker.make("Notification", creator=user)
         request.data = {"received_notifications": [notification.id]}
         api_view = ViewedReceivedNotifications()
 

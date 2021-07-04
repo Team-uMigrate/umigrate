bc4f8b6ece7262a905735f72cb9335ad3dd4ff5d
Shashank Anand <anandkannan.shashank62@gmail.com>
2021-07-04 16:00:05 -0400

    Restore ProfilePhotoView to be in line with pre-existing components

--- app/src/src/components/views/ProfilePhotoView.jsx
+++ app/src/src/components/views/ProfilePhotoView.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import { Avatar } from 'react-native-paper';
 
 // Both Avatar.Icon and Avatar.Image's style objects accept the same attributes as Views' style objects
-const ProfilePhotoView = ({ photo, style = {}, size = 45 }) => {
+const ProfilePhotoView = ({ photo, styles, size = 45 }) => {
   if (photo === null) {
-    return <Avatar.Icon size={size} icon={'account'} style={style} />;
+    return <Avatar.Icon size={size} icon={'account'} style={styles} />;
   } else {
-    return <Avatar.Image size={size} source={{ uri: photo }} style={style} />;
+    return <Avatar.Image size={size} source={{ uri: photo }} style={styles} />;
   }
 };
 

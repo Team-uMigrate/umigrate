ce4860546a94b360b6417b01a1dce116459b7b02
Vansh Mago <vmago@uwaterloo.ca>
2021-07-07 23:58:26 -0400

    InitialCommit

--- app/src/src/screens/likes/LikesScreen.jsx
+++ app/src/src/screens/likes/LikesScreen.jsx
@@ -1,15 +1,17 @@
 import { ListingsEndpoint, PostsEndpoint } from '../../utils/endpoints';
 import ListingView from '../../components/views/ListingView';
-import React, { useRef, useState } from 'react';
+import React, { useRef, useContext, useState } from 'react';
 import { useScrollToTop } from '@react-navigation/native';
-import { View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import Header from '../../components/views/Header';
 import FeedContainer from '../../components/containers/FeedContainer';
 import CreateItemModal from '../../components/modals/CreateItemModal';
+import StackNavContext from '../../contexts/StackNavContext';
 import { sharedLikesCommentsstyles } from '../../stylesheets/likesAndComments/likesAndComments.jsx';
 import { StackNavigationProp } from '@react-navigation/stack';
-import { RouteProp } from '@react-navigation/native';
+// import { RouteProp } from '@react-navigations/native';
 import UserView from '../../components/views/UserView';
+import { IconButton, Searchbar } from 'react-native-paper';
 
 const itemViews = [
   (item, updateItem) => <UserView item={item} updateItem={updateItem} />,
@@ -28,13 +30,23 @@ const LikesScreen = ({ navigation, route }) => {
       await PostsEndpoint.likes(route.params['postId'], page),
   ];
 
+  const nav = useContext(StackNavContext);
+
   const ref = useRef(null);
 
   useScrollToTop(ref);
 
   return (
     <View style={sharedLikesCommentsstyles.container}>
-      <Header title="Likes" />
+      <View style={{ flexDirection: 'row' }}>
+        <IconButton
+          icon="arrow-left"
+          style={styles.button}
+          size={20}
+          onPress={() => nav.navigation.goBack()}
+        />
+        <Text style={styles.text}>Liked Users</Text>
+      </View>
       <FeedContainer
         fetchItemsList={fetchItemsList}
         itemViews={itemViews}
@@ -45,5 +57,19 @@ const LikesScreen = ({ navigation, route }) => {
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 27,
+    marginTop: 60,
+    marginLeft: 40,
+  },
+
+  button: {
+    color: 'black',
+    marginTop: 62,
+    marginLeft: 10,
+  },
+});
 
 export default LikesScreen;

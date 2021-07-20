82f575d4d7d518657a48a78016482251f42b1b86
Vansh Mago <vmago@uwaterloo.ca>
2021-07-20 13:25:48 -0400

    "Commenting changes not part of this ticket"

--- app/src/src/screens/menu/SavedItemsScreen.jsx
+++ app/src/src/screens/menu/SavedItemsScreen.jsx
@@ -1,22 +1,22 @@
-import React from 'react';
-import { Text, View } from 'react-native';
-import Header from '../../components/views/Header';
-import { Button } from 'react-native-paper';
-import { routes } from '../../utils/routes';
-import { sharedMenustyles } from '../../stylesheets/menu/menu.jsx';
+// import React from 'react';
+// import { Text, View } from 'react-native';
+// import Header from '../../components/views/Header';
+// import { Button } from 'react-native-paper';
+// import { routes } from '../../utils/routes';
+// import { sharedMenustyles } from '../../stylesheets/menu/menu.jsx';
 
-const SavedItemsScreen = ({ navigation }) => {
-  const menuRedirect = () => {
-    navigation.navigate(routes.menuHome);
-  };
+// const SavedItemsScreen = ({ navigation }) => {
+//   const menuRedirect = () => {
+//     navigation.navigate(routes.menuHome);
+//   };
 
-  return (
-    <View style={sharedMenustyles.container}>
-      <Header title="Saved Posts" />
-      <Text style={sharedMenustyles.title}>Saved Posts Page!</Text>
-      <Button onPress={menuRedirect}>Back to Menu</Button>
-    </View>
-  );
-};
+//   return (
+//     <View style={sharedMenustyles.container}>
+//       <Header title="Saved Posts" />
+//       <Text style={sharedMenustyles.title}>Saved Posts Page!</Text>
+//       <Button onPress={menuRedirect}>Back to Menu</Button>
+//     </View>
+//   );
+// };
 
-export default SavedItemsScreen;
+// export default SavedItemsScreen;
